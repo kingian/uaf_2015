64a45d9e35b1399303bfdea43bcc79441bfb6478
Ian King <gnik.nai@gmail.com>
2015-01-18 19:28:18 -0700

    Restructured folders.

---
+++ unity_parse/jesses_code/Assets/PagingInfo.cs
@@ -0,0 +1,10 @@
+﻿public class PagingInfo : object {
+
+	public int PageIndex = 0;
+	public int PageSize = 1000;
+
+	public int SkipAmount {
+		get {return this.PageSize * this.PageIndex; }
+	}
+
+}
