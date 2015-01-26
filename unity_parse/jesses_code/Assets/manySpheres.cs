0690131415b4d4221ed8d5bc404d1d351e8e87c6
macmann <macmann@macmanns-MacBook-Pro.local>
2015-01-25 23:26:36 -0700

    updates manySPheres to create in bounds, and count. 7.6 million polys at 20fps

--- unity_parse/jesses_code/Assets/manySpheres.cs
+++ unity_parse/jesses_code/Assets/manySpheres.cs
@@ -4,42 +4,46 @@ using System.Collections.Generic;
 
 public class manySpheres : MonoBehaviour {
 
-	public float scaler = 10000;
+	private float bounds = 100;
+	private int total_objects = 10000;
 
-	public LinkedList<Vector3> jessesList;
+	private LinkedList<GameObject> jessesList;
 
 	// Use this for initialization
 	void Start () 
 	{
-		jessesList = new LinkedList<Vector3> ();
-		for (int j=0; j <= 10; j++) 
-		{
-			makeAK();
-			
-			//			while (j<=100)yield return new WaitForSeconds (5.0f);
-		}
+		jessesList = new LinkedList<GameObject> ();
+		makeAK();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-
-	
+		foreach( var game_object in this.jessesList){
+			game_object.transform.Translate(.1f,0,.1f);
+		}
 	}
 
 	void makeAK()
-	{
-		for (int i=0; i <=1000; i++) {
-						GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
-						sphere.transform.parent = this.transform;
-						sphere.transform.position = randomVector();
-						sphere.renderer.material.shader = Shader.Find ("Transparent/Diffuse");
-						sphere.renderer.material.color = new Color (0, 1.0f, .5f, .25f);
-				}
+	{	
+		var numTriangles = 0;
+		for (int i=0; i <= this.total_objects; i++) {
+			GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+			sphere.transform.parent = this.transform;
+			sphere.transform.position = randomVector();
+			sphere.renderer.material.shader = Shader.Find ("Transparent/Diffuse");
+			sphere.renderer.material.color = new Color (1.0f,0, 0, 1.0f);
+			this.jessesList.AddLast(sphere);
+
+			numTriangles = sphere.GetComponent<MeshFilter>().mesh.triangles.Length/3;
+		}
+		Debug.Log (this.jessesList.Count);
+		Debug.Log(numTriangles);
+		Debug.Log(numTriangles*this.total_objects);
 	}
 
 	Vector3 randomVector()
 	{
-		return new Vector3 ((Random.value - 0.5f) * scaler, 1.0f, (Random.value - 0.5f) * scaler);
+		return new Vector3 (Random.Range(-this.bounds, this.bounds), 1.0f, Random.Range(-this.bounds, this.bounds));
 	}
 }
